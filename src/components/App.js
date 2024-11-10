7917cf669eb852e0c4c63211bd42b85536722140
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-11-10 17:54:29 +0300

    displays plants on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => {
+        setPlants(data);
+      });
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} />
     </div>
   );
 }
