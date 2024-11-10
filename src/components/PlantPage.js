a3f42cfa3eb0c7e21d4c52a12d287a7fbe2d14c8
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-11-10 18:27:52 +0300

    adds add search feature

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,13 +2,20 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useState } from "react";
 
 function PlantPage({plants, addPlant}) {
+  const [search, setSearch] = useState("");
+
+  const filter = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
       <NewPlantForm addPlant={addPlant} />
-      <Search />
-      <PlantList plants={plants} />
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={filter} />
     </main>
   );
 }
