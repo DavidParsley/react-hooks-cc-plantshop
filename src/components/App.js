4a8635d97bc41a69850a28c46ca0127ebb579be1
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-11-10 18:03:09 +0300

    adds add plant feature

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import { useEffect, useState } from "react";
 function App() {
   const [plants, setPlants] = useState([]);
 
+  function addPlant(newPlant) {
+    setPlants((plants) => [...plants, newPlant]);
+  }
+
   useEffect(() => {
     fetch("http://localhost:6001/plants")
       .then((response) => response.json())
@@ -17,7 +21,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <PlantPage plants={plants} />
+      <PlantPage plants={plants} addPlant={addPlant} />
     </div>
   );
 }
