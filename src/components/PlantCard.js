ab39261767b0f70c4c2042e107d7c0af1661bb92
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-11-10 17:57:59 +0300

    button / sold out feature

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 function PlantCard({plant}) {
+  const [inStock, setInStock] = useState(true);
+  const color = inStock ? "green" : "grey";
+
   return (
     <li className="card" data-testid="plant-item">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <button className="primary" style={{ background: color }} onClick={() => setInStock(!inStock)}>{inStock ? "In Stock" : "Out of Stock"}</button>
     </li>
   );
 }
